7df334c831ca693916023b1d29d4651079a7cc5f
Oleg Grekov <broGrekov@gmail.com>
2025-03-03 21:46:59 +0200

    add main page

---
+++ script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    // navigation: {
+    //   nextEl: ".swiper-button-next",
+    //   prevEl: ".swiper-button-prev",
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
